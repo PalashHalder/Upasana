b1ba8a828b33a1891ace477724a65a5d84dcfde9
PalashHalder <halder.palash08@gmail.com>
2022-08-14 17:25:03 +0600

    activity main design update done.

--- app/src/main/java/info/palashhalder/upasana/MainActivity.java
+++ app/src/main/java/info/palashhalder/upasana/MainActivity.java
@@ -1,14 +1,19 @@
 package info.palashhalder.upasana;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/info/palashhalder/upasana/SplashActivity.java
+++ app/src/main/java/info/palashhalder/upasana/SplashActivity.java
@@ -2,13 +2,35 @@ package info.palashhalder.upasana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
+
+    CountDownTimer countDownTimer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        countDownTimer = new CountDownTimer(5000,100) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(),(MainActivity.class));
+
+                startActivity(intent);
+                finish();
+            }
+        }.start();
     }
 }
