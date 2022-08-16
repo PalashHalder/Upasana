418c294d75b3a70f7999ef8936a4178e53f63928
PalashHalder <halder.palash08@gmail.com>
2022-08-16 17:33:18 +0600

    icon navigation index and guruinvoke activity index and guruinvoke layout added

--- app/src/main/java/info/palashhalder/upasana/MainActivity.java
+++ app/src/main/java/info/palashhalder/upasana/MainActivity.java
@@ -2,17 +2,53 @@ package info.palashhalder.upasana;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnIndex, btnGuruInvoke, btnToMind, btnHymn, btnPrayer, btnThanks, btnBow, btnSatyaDharma, btnWorship, btnSadhana, btnGuruLife, btnMessage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIndex = findViewById(R.id.btnIndex);
+        btnGuruInvoke = findViewById(R.id.btnGuruInvoke);
+        btnToMind = findViewById(R.id.btnToMind);
+        btnHymn = findViewById(R.id.btnHymn);
+        btnPrayer = findViewById(R.id.btnPrayer);
+        btnThanks = findViewById(R.id.btnThanks);
+        btnBow = findViewById(R.id.btnBow);
+        btnSatyaDharma = findViewById(R.id.btnSatyaDharma);
+        btnWorship = findViewById(R.id.btnWorship);
+        btnSadhana = findViewById(R.id.btnSadhana);
+        btnGuruLife = findViewById(R.id.btnGuruLife);
+        btnMessage = findViewById(R.id.btnMessage);
+
+        btnIndex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, IndexActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGuruInvoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, GuruInvokeActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
